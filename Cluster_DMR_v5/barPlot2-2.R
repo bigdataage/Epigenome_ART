bbbcaa8d210cbab93c8bb370e62ae4bc3520cda0
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2018-04-22 15:00:13 +0800

    Add files via upload

---
+++ Cluster_DMR_v5/barPlot2-2.R
@@ -0,0 +1,142 @@
+library("ggplot2") 
+library("reshape2") 
+library("psych")
+library("minerva")
+library(plyr) 
+
+MyTheme_1 <- function(textSize1=14, hjust1=1, vjust1=1,  angle1=30) {    # "hjust=1, vjust=1, angle=30" for some boxplots.
+  theme(  
+    line  = element_line(colour="black",  size=1.0,   linetype=1,      lineend=NULL),                                                                                        ## all line elements.          局部优先总体,下面3个也是,只对非局部设置有效.   所有线属性.
+    rect  = element_rect(colour="black",  size=1.0,   linetype=1,      fill="transparent" ),                                                                                 ## all rectangluar elements.    hjust=1: 靠右对齐.   所有矩形区域属性.
+    text  = element_text(family="serif",  face="plain",  colour="black",  size=textSize1, hjust=0.5, vjust=0.5,   angle=0, lineheight=1.0,  margin = NULL, debug = NULL),    ## all text elements.           "serif" for a serif font. 所有文本相关属性.
+    title = element_text(family="serif",  face="plain",  colour="black",  size=textSize1, hjust=0.5, vjust=0.5,   angle=0, lineheight=1.0,  margin = NULL, debug = NULL),    ## all title elements: plot, axes, legends.    hjust:水平对齐的方向.  所有标题属性.
+    ## aspect.ratio = 1,   ##高宽比
+    
+    axis.title    = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=0.5,    vjust=0.5,    angle=0,       lineheight=1.0,  margin = NULL, debug = NULL),       ## label of axes (element_text; inherits from text).  horizontal: 水平的, 水平线 
+    axis.title.x  = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=0.5,    vjust=0.5,    angle=0,       lineheight=1.0,  margin = NULL, debug = NULL),       ## x axis label (element_text; inherits from axis.title)
+    axis.title.y  = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=0.5,    vjust=0.5,    angle=90,      lineheight=1.0,  margin = NULL, debug = NULL),       ## y axis label (element_text; inherits from axis.title)
+    axis.text     = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=0.5,    vjust=0.5,    angle=0,       lineheight=1.0,  margin = NULL, debug = NULL),       ## tick labels along axes (element_text; inherits from text). 坐标轴刻度的标签的属性.                                                         
+    axis.text.x   = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=hjust1, vjust=vjust1, angle=angle1,  lineheight=1.0,  margin = NULL, debug = NULL),       ## x axis tick labels (element_text; inherits from axis.text)
+    axis.text.y   = element_text(family="serif", face="plain", colour="black", size=textSize1,    hjust=0.5,    vjust=0.5,    angle=0,       lineheight=1.0,  margin = NULL, debug = NULL),       ## y axis tick labels (element_text; inherits from axis.text)
+    
+    axis.ticks        = element_line(colour="black", size=0.5, linetype=1, lineend=NULL),          ## tick marks along axes (element_line; inherits from line). 坐标轴刻度线.
+    axis.ticks.x      = element_line(colour="black", size=0.5, linetype=1, lineend=NULL),          ## x axis tick marks (element_line; inherits from axis.ticks)
+    axis.ticks.y      = element_line(colour="black", size=0.5, linetype=1, lineend=NULL),          ## y axis tick marks (element_line; inherits from axis.ticks)
+    axis.ticks.length = grid::unit(2.0,   "mm",   data=NULL),                                      ## length of tick marks (unit), ‘"mm"’ Millimetres.  10 mm = 1 cm.  刻度线长度
+    axis.line         = element_line(colour="transparent", size=0.3, linetype=1, lineend=NULL), 	 ## lines along axes (element_line; inherits from line). 坐标轴线
+    axis.line.x       = element_line(colour="transparent", size=0.3, linetype=1, lineend=NULL), 	 ## line along x axis (element_line; inherits from axis.line)
+    axis.line.y       = element_line(colour="transparent", size=0.3, linetype=1, lineend=NULL),	   ## line along y axis (element_line; inherits from axis.line)
+    
+    legend.background    = element_rect(colour="transparent", size=1, linetype=1, fill="transparent" ), 	      ## background of legend (element_rect; inherits from rect)
+    legend.spacing       = grid::unit(1, "mm", data=NULL), 	                                                    ## extra space added around legend (unit). linetype=1指的是矩形边框的类型.
+    legend.key           = element_rect(colour="transparent", size=2, linetype=1, fill="transparent" ), 	      ## background underneath legend keys. 图例符号. size=1指的是矩形边框的大小.
+    legend.key.size      = grid::unit(6,   "mm", data=NULL) , 	                                                ## size of legend keys   (unit; inherits from legend.key.size)
+    legend.key.height    = grid::unit(6.5, "mm", data=NULL) , 	                                                ## key background height (unit; inherits from legend.key.size)
+    legend.key.width     = grid::unit(8,   "mm", data=NULL) ,                                                   ## key background width  (unit; inherits from legend.key.size)
+    legend.text          = element_text(family="serif", face=NULL, colour="black", size=textSize1, hjust=NULL, vjust=NULL, angle=NULL, lineheight=NULL), 	##legend item labels. 图例文字标签.
+    legend.text.align    = 0, 	                    ## alignment of legend labels (number from 0 (left) to 1 (right))
+    legend.title         = element_blank(),   	    ## title of legend (element_text; inherits from title)
+    legend.title.align   = 0, 	                    ## alignment of legend title (number from 0 (left) to 1 (right))
+    legend.position      = "right", 	              ## the position of legends. ("left", "right", "bottom", "top", or two-element numeric vector)
+    legend.direction     = "vertical",        	    ## layout of items in legends  ("horizontal" or "vertical")   图例排列方向
+    legend.justification = "center",      	        ## anchor point for positioning legend inside plot ("center" or two-element numeric vector)  图例居中方式
+    legend.box           = NULL, 	                  ## arrangement of multiple legends ("horizontal" or "vertical")  多图例的排列方式
+    legend.box.just      = NULL, 	                  ## justification of each legend within the overall bounding box, when there are multiple legends ("top", "bottom", "left", or "right")  多图例的居中方式
+    
+    panel.background   = element_rect(colour="transparent", size=0.0, linetype=1, fill="transparent" ),   	## background of plotting area, drawn underneath plot (element_rect; inherits from rect)
+    panel.border       = element_rect(colour="black", size=0.5, linetype=1, fill=NA ), 	                    ## border around plotting area, drawn on top of plot so that it covers tick marks and grid lines. This should be used with fill=NA (element_rect; inherits from rect)
+    panel.spacing      = grid::unit(1, "mm", data=NULL) , 	                                                ## margin around facet panels (unit)  分面绘图区之间的边距
+    panel.spacing.x    = grid::unit(1, "mm", data=NULL) ,
+    panel.spacing.y    = grid::unit(1, "mm", data=NULL) ,
+    panel.grid         = element_blank(), 	                                                                ## grid lines (element_line; inherits from line)  绘图区网格线
+    panel.grid.major   = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) , 	    ## major grid lines (element_line; inherits from panel.grid)  主网格线
+    panel.grid.minor   = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) ,     	## minor grid lines (element_line; inherits from panel.grid)  次网格线
+    panel.grid.major.x = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) , 	    ## vertical major grid lines (element_line; inherits from panel.grid.major)
+    panel.grid.major.y = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) ,     	## horizontal major grid lines (element_line; inherits from panel.grid.major)
+    panel.grid.minor.x = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) ,     	## vertical minor grid lines (element_line; inherits from panel.grid.minor)
+    panel.grid.minor.y = element_line(colour="transparent", size=NULL, linetype=NULL, lineend=NULL) ,     	## horizontal minor grid lines (element_line; inherits from panel.grid.minor)
+    
+    plot.background	= element_rect(colour="transparent", size=NULL, linetype=NULL, fill="transparent" ),                                                ## background of the entire plot (element_rect; inherits from rect)  整个图形的背景
+    plot.title      = element_text(family="serif", face=NULL, colour="black", size=textSize1, hjust=0.5, vjust=0.5,   angle=NULL, lineheight=NULL),     ## plot title (text appearance) (element_text; inherits from title)  图形标题
+    plot.margin     = grid::unit(c(5, 5, 5, 5), "mm", data=NULL), 	                                                                                    ## margin around entire plot (unit with the sizes of the top, right, bottom, and left margins)
+    
+    strip.background = element_rect(colour=NULL,    size=NULL, linetype=NULL, fill=NULL ), 	                                                      ## background of facet labels (element_rect; inherits from rect)  分面标签背景
+    strip.text       = element_text(family="serif", face=NULL, colour=NULL, size=NULL, hjust=NULL, vjust=NULL, angle=NULL, lineheight=NULL), 	    ## facet labels (element_text; inherits from text)
+    strip.text.x     = element_text(family="serif", face=NULL, colour=NULL, size=NULL, hjust=NULL, vjust=NULL, angle=NULL, lineheight=NULL), 	    ## facet labels along horizontal direction (element_text; inherits from strip.text)
+    strip.text.y     = element_text(family="serif", face=NULL, colour=NULL, size=NULL, hjust=NULL, vjust=NULL, angle=NULL, lineheight=NULL)   	  ## facet labels along vertical direction (element_text; inherits from strip.text) 
+  ) 
+} 
+
+
+MySaveGgplot2_1 <- function(ggplot2Figure1,  path1, fileName1,  height1, width1) {
+  SVG1 <- paste(path1,  "/",  "SVG",  sep = "",  collapse = NULL)
+  PNG1 <- paste(path1,  "/",  "PNG",  sep = "",  collapse = NULL)
+  PDF1 <- paste(path1,  "/",  "PDF",  sep = "",  collapse = NULL)
+  EPS1 <- paste(path1,  "/",  "EPS",  sep = "",  collapse = NULL)
+  if( ! file.exists(SVG1) ) { dir.create(SVG1) }
+  if( ! file.exists(PNG1) ) { dir.create(PNG1) }
+  if( ! file.exists(PDF1) ) { dir.create(PDF1) }
+  if( ! file.exists(EPS1) ) { dir.create(EPS1) }
+  ggsave( filename = paste(SVG1,  "/",  fileName1,  ".svg",  sep="",  collapse=NULL),     height=height1,    width=width1,      dpi = 1200 )
+  ggsave( filename = paste(PNG1,  "/",  fileName1,  ".png",  sep="",  collapse=NULL),     height=height1,    width=width1,      dpi = 1200 )
+  ggsave( filename = paste(PDF1,  "/",  fileName1,  ".pdf",  sep="",  collapse=NULL),     height=height1,    width=width1,      dpi = 1200 )
+  ggsave( filename = paste(EPS1,  "/",  fileName1,  ".eps",  sep="",  collapse=NULL),     height=height1,    width=width1,      dpi = 1200,   device=cairo_ps)         
+}
+
+ 
+
+
+###########################################################################################################################################################################
+rawMatrix_1 <- read.table("diff2_number.txt", header=FALSE,   sep="\t" )  
+dim(rawMatrix_1 )
+rawMatrix_1[1:3,]
+
+
+
+
+matrix1 = rawMatrix_1[2:11, 3:4]
+colName1 = as.vector(unlist( rawMatrix_1[1, 3:4] ))
+rowName1a = as.vector(unlist( rawMatrix_1[2:11, 1] ))
+rowName1b = as.vector(unlist( rawMatrix_1[2:11, 2] ))
+
+
+
+
+
+
+
+DataFrame_Local1 <- data.frame( xAxis = rep( colName1 , each = nrow(matrix1) ) , 
+                               yAxis  = as.numeric( as.character ( unlist( matrix1 )) ), 
+                               SampleType1 = rep( rowName1a , ncol(matrix1) ),
+                               SampleType2 = rep( rowName1b , ncol(matrix1) )
+)
+
+ 
+
+
+
+AllResults_g <- "Z-FinalFigures"
+if( ! file.exists(AllResults_g) ) { dir.create(path=AllResults_g, recursive = TRUE) }
+
+
+FigureTemp1 <- ggplot(DataFrame_Local1, aes(x= SampleType1 , fill=SampleType2, y=yAxis ) )  +
+  geom_bar( stat="identity"    ) +  facet_grid(. ~ xAxis) +
+  xlab("Samples") + ylab("Number of DMCs") +  MyTheme_1(textSize=12 )   
+MySaveGgplot2_1(ggplot2Figure1=FigureTemp1,  path1=AllResults_g , fileName1="diff2-1-a",  height1=4,  width1=6)
+
+ 
+
+
+FigureTemp1 <- ggplot(DataFrame_Local1, aes(x=xAxis  , fill=SampleType2, y=yAxis ) )  +
+  geom_bar( stat="identity"    ) +  facet_grid(. ~ SampleType1) +
+  xlab("Samples") + ylab("Number of DMCs") +  MyTheme_1(textSize=12 )   
+MySaveGgplot2_1(ggplot2Figure1=FigureTemp1,  path1=AllResults_g , fileName1="diff2-1-b",  height1=4,  width1=6)
+
+ 
+
+
+
+
+
+
+
